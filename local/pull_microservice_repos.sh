a0478e9d677bb52d29dd7676a533147d30ef3551
Charles Blount <mathfan1980@gmail.com>
2020-05-19 17:47:16 -0400

    added kinesis ambassador

---
+++ local/pull_microservice_repos.sh
@@ -0,0 +1,5 @@
+git clone git@github.com:cb4000/kinesis-bi-ambassador.git &
+git clone git@github.com:cb4000/node-lexical-island.git &
+git clone git@github.com:cb4000/reflexive-persistance.git &
+git clone git@github.com:cb4000/pink-percival.git 
+
